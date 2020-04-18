df21a5c1cba5bcf5d56c79c94d8ab43396cfb06b
Em Ji Madhu <emjimadhu@gmail.com>
2020-04-19 01:49:40 +0530

    refactor(eslint): Add additional eslint plugins
    1) Add `unicorn`, `import`, `jest` and `jest-formatting` eslint plugins
    2) Add rules for added plugins and fixes linting issues
    3) Add `pre-commit` git hook to `husky`

--- src/libs/sample/index.spec.ts
+++ src/libs/sample/index.spec.ts
@@ -1,6 +1,7 @@
+import * as path from 'path';
+
 import { Tree } from '@angular-devkit/schematics';
 import { SchematicTestRunner } from '@angular-devkit/schematics/testing';
-import * as path from 'path';
 
 
 const collectionPath = path.join(__dirname, '../../collection.json');
@@ -8,11 +9,13 @@ const collectionPath = path.join(__dirname, '../../collection.json');
 
 describe('schematics sample', () => {
   it('works', () => {
+    expect.assertions(1);
+
     const runner = new SchematicTestRunner('schematics', collectionPath);
     const tree = runner.runSchematic('sample', {
       name: 'foo'
     }, Tree.empty());
 
-    expect(tree.files).toEqual([]);
+    expect(tree.files).toStrictEqual([]);
   });
 });
