4d80eadca903d510b96039d85df71ce46e057459
Em Ji Madhu <emjimadhu@gmail.com>
2020-04-19 01:25:24 +0530

    refactor(eslint): add eslint support
    1) Add eslint and eslint typescript plugins,
    2) Add eslintrc.json for specify eslint rules,
    3) Add script to lint files

--- src/libs/sample/index.ts
+++ src/libs/sample/index.ts
@@ -1,9 +1,11 @@
-import { Rule, SchematicContext, Tree } from '@angular-devkit/schematics';
+import {
+  Rule, SchematicContext, Tree
+} from '@angular-devkit/schematics';
 
-import {ISampleOptions} from "./schema";
+import { ISampleOptions } from './schema';
 
 export function sample(_options: ISampleOptions): Rule {
-  return (tree: Tree, _context: SchematicContext) => {
+  return (tree: Tree, _context: SchematicContext): Tree => {
     return tree;
   };
 }
